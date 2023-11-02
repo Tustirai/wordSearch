837cca476894f4d1c73bd9b3688c2d917df9b886
Audrey Huvaya <135869085+Tustirai@users.noreply.github.com>
2023-11-02 19:23:33 +0200

    JS update
    fill in empty spaces with random words

--- src/script.js
+++ src/script.js
@@ -31,6 +31,7 @@ function arrangeGame() {
 	const rows = 14;
 	const cols = 14;
 	let html = "";
+
 	for (let i = 0; i < rows; i++) {
 		html += "<div class='row'>";
 		for (let j = 0; j < cols; j++) {
@@ -38,9 +39,11 @@ function arrangeGame() {
 		}
 		html += "</div>";
 	}
+
 	puzzle.innerHTML = html;
 
 	placeLetters(words);
+	fillEmptySpaces();
 
 	function placeLetters(myArr) {
 		const positions = ["rows", "column", "diagonal"];
@@ -49,9 +52,11 @@ function arrangeGame() {
 			let orientation =
 				positions[Math.floor(Math.random() * positions.length)];
 			let placed = false;
+
 			for (let j = 0; j < 100 && !placed; j++) {
 				let startRow = Math.floor(Math.random() * rows);
 				let startCol = Math.floor(Math.random() * cols);
+
 				if (canPlaceWord(myArr[i], startRow, startCol, orientation)) {
 					placeWord(myArr[i], startRow, startCol, orientation);
 					placed = true;
@@ -62,37 +67,46 @@ function arrangeGame() {
 
 	function canPlaceWord(word, startRow, startCol, orientation) {
 		const wordLength = word.length;
+
 		switch (orientation) {
 			case "rows":
 				if (startCol + wordLength > cols) {
 					return false;
 				}
+
 				for (let i = 0; i < wordLength; i++) {
 					let box = document.querySelector(
 						`.singleWord[data-row='${startRow}'][data-col='${
 							startCol + i
 						}']`
 					);
+
 					if (box.textContent !== "" && box.textContent !== word[i]) {
 						return false;
 					}
 				}
+
 				return true;
+
 			case "column":
 				if (startRow + wordLength > rows) {
 					return false;
 				}
+
 				for (let i = 0; i < wordLength; i++) {
 					let box = document.querySelector(
 						`.singleWord[data-row='${
 							startRow + i
 						}'][data-col='${startCol}']`
 					);
+
 					if (box.textContent !== "" && box.textContent !== word[i]) {
 						return false;
 					}
 				}
+
 				return true;
+
 			case "diagonal":
 				if (
 					startRow + wordLength > rows ||
@@ -100,22 +114,26 @@ function arrangeGame() {
 				) {
 					return false;
 				}
+
 				for (let i = 0; i < wordLength; i++) {
 					let box = document.querySelector(
 						`.singleWord[data-row='${startRow + i}'][data-col='${
 							startCol + i
 						}']`
 					);
+
 					if (box.textContent !== "" && box.textContent !== word[i]) {
 						return false;
 					}
 				}
+
 				return true;
 		}
 	}
 
 	function placeWord(word, startRow, startCol, orientation) {
 		const wordLength = word.length;
+
 		switch (orientation) {
 			case "rows":
 				for (let i = 0; i < wordLength; i++) {
@@ -124,10 +142,13 @@ function arrangeGame() {
 							startCol + i
 						}']`
 					);
+
 					box.textContent = word[i];
 					box.style.whiteSpace = "nowrap";
 				}
+
 				break;
+
 			case "column":
 				for (let i = 0; i < wordLength; i++) {
 					let box = document.querySelector(
@@ -135,10 +156,13 @@ function arrangeGame() {
 							startRow + i
 						}'][data-col='${startCol}']`
 					);
+
 					box.textContent = word[i];
 					box.style.whiteSpace = "nowrap";
 				}
+
 				break;
+
 			case "diagonal":
 				for (let i = 0; i < wordLength; i++) {
 					let box = document.querySelector(
@@ -146,10 +170,25 @@ function arrangeGame() {
 							startCol + i
 						}']`
 					);
+
 					box.textContent = word[i];
 					box.style.whiteSpace = "nowrap";
 				}
+
 				break;
 		}
+	}
+
+	function fillEmptySpaces() {
+		const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		const emptySpaces = document.querySelectorAll(".singleWord:empty");
+
+		emptySpaces.forEach((space) => {
+			const randomLetter = letters.charAt(
+				Math.floor(Math.random() * letters.length)
+			);
+
+			space.textContent = randomLetter;
+		});
 	}
 }
